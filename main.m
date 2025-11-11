c4b814589786f46da452d6f6c6702fd9acbcd0af
DanielGruenCU <dagr2769@colorado.edu>
2025-11-11 12:57:59 -0600

    Update main.m

--- main.m
+++ main.m
@@ -1,2 +1,26 @@
-clear clc;
+clear all;
 clc;
+
+problem_1 = 1; % on or off 1 or 0
+problem_2 = 1; % on or off 1 or 0
+problem_3 = 1; % on or off 1 or 0
+
+
+%% Problem 1
+if (problem_1)
+
+
+
+end
+%% Problem 2
+if (problem_2)
+
+
+
+end
+%% Problem 3
+if (problem_3)
+
+
+
+end
